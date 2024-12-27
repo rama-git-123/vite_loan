0fb0f575be6fd7e3bce6e2b139c8adadef2fb651
Ramakanta Sahoo <Ramakanta.Sahoo1@ibm.com>
2024-12-27 14:14:10 +0530

    initial push

---
+++ y/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1> Welcome to Vite + React</h1>
+      
+      <p className="read-the-docs">
+       we can start here loan project 
+      </p>
+    </>
+  )
+}
+
+export default App
