a1d17060e8d29d917a8785ba64e96b303a4f7df0
Ramakanta Sahoo <Ramakanta.Sahoo1@ibm.com>
2025-02-11 23:30:51 +0530

    cdn path change

--- y/src/main.jsx
+++ y/src/main.jsx
@@ -4,7 +4,8 @@ import "./index.css";
 import App from "./App.jsx";
 import "../src/scss/style.scss";
 import * as bootstrap from "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+//import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
